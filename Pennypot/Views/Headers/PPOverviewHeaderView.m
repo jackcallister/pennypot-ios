192aa99224ae98e56a51444f030610c61ea828fa
Mark Feaver <mark.feaver@gmail.com>
2015-04-22 22:49:27 +1200

    Fixes #3 - Frame skipping on Tableview header parallax scroll

--- Pennypot/Views/Headers/PPOverviewHeaderView.m
+++ Pennypot/Views/Headers/PPOverviewHeaderView.m
@@ -77,26 +77,22 @@ static const CGFloat kPadding = 20.0f;
     
     if (offset.y > 0) {
         backgroundFrame.origin.y = MAX(offset.y *kParallaxDeltaFactor, 0);
-        
+        backgroundFrame.size.height = kDefaultHeaderFrame.size.height;
         
         self.backgroundScrollView.frame = backgroundFrame;
         
         self.blurredImageView.alpha = (1 / kDefaultHeaderFrame.size.height * offset.y * 2);
         
         self.clipsToBounds = YES;
-    
     } else {
-        
-        CGFloat delta = 0.0f;
         CGRect defaultFrame = kDefaultHeaderFrame;
         
-        delta = fabs(MIN(0.0f, offset.y));
+        CGFloat delta = fabs(MIN(0.0f, offset.y));
         defaultFrame.origin.y -= delta;
         defaultFrame.size.height += delta;
         
         self.backgroundScrollView.frame = defaultFrame;
         self.clipsToBounds = NO;
-        
     }
 }
 
